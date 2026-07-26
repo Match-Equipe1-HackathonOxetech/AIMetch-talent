ff257a3485825bfe9860fc60a317ca11b4e39dbb
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-07-26 12:08:12 -0300

    Update app.run to use environment variables

--- run.py
+++ run.py
@@ -6,5 +6,11 @@ from app import criar_app
 
 app = criar_app()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=os.environ.get("FLASK_ENV") == "development"
+    )
